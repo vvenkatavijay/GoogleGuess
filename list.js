ceeac8bd2e70f138fa9267288549a8914aece245
vvenkatavijay <root@Venkatas-MacBook-Pro.local>
2015-12-04 23:55:04 -0800

    first commit. Basic functioning works

---
+++ list.js
@@ -0,0 +1,10 @@
+var questions_people = '{ "questions" : [' +
+'{ "question":"Rahul" , "1":"Gandhi", "2":"Yadav", "3": "Dravid", "4":"Sharma" },' +
+  '{ "question":"Sachin" , "1":"Tendulkar", "2":"Jigar", "3": "Pilot", "4":"Bansal" }]}';
+
+
+var questions_places = '{ "questions" : [' +
+'{ "question":"Rahul" , "1":"Gandhi", "2":"Yadav", "3": "Dravid", "4":"Sharma" },' +
+  '{ "question":"Sachin" , "1":"Tendulkar", "2":"Jigar", "3": "Pilot", "4":"Bansal" }]}';
+
+  var questions = '{"I hate it when", "Why is Salman Khan", "Why is Deepika"}';
