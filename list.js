725ffd6b3715f6019380153a205f9e17e1355fc1
vvenkatavijay <root@Venkatas-MacBook-Pro.local>
2015-12-08 09:28:57 -0800

    added real time autocomplete feature

--- list.js
+++ list.js
@@ -7,4 +7,6 @@ var questions_places = '{ "questions" : [' +
 '{ "question":"Rahul" , "1":"Gandhi", "2":"Yadav", "3": "Dravid", "4":"Sharma" },' +
   '{ "question":"Sachin" , "1":"Tendulkar", "2":"Jigar", "3": "Pilot", "4":"Bansal" }]}';
 
-  var questions = '{"I hate it when", "Why is Salman Khan", "Why is Deepika"}';
+  var questions_api_people = ["Sachin", "Rahul", "Aamir", "Sanjay", "Abdul", "Siddharth"];
+
+  var questions_api_question = ["I hate it when", "Why is", "What is the biggest"];
