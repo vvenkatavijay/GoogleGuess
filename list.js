a909c94276d120fdd7686e8e164b4c92953ad06e
vvenkatavijay <root@Venkatas-MacBook-Pro.local>
2015-12-12 19:24:25 -0800

    fixing the repeatitive match answer

--- list.js
+++ list.js
@@ -7,6 +7,8 @@ var questions_places = '{ "questions" : [' +
 '{ "question":"Rahul" , "1":"Gandhi", "2":"Yadav", "3": "Dravid", "4":"Sharma" },' +
   '{ "question":"Sachin" , "1":"Tendulkar", "2":"Jigar", "3": "Pilot", "4":"Bansal" }]}';
 
-  var questions_api_people = ["Sachin", "Rahul", "Aamir", "Sanjay", "Abdul", "Siddharth"];
+var questions_api_people = ["Sachin", "Rahul", "Aamir", "Sanjay", "Abdul", "Siddharth"];
 
-  var questions_api_question = ["I hate it when", "Why is", "What is the biggest", "Who is", "why is bollywood", "why is cricket"];
+var questions_api_question = ["I hate it when", "Why is", "What is the biggest", "Who is", "why is bollywood", "why is cricket"];
+
+var skip_words = ["THE", "IS", "TO", "SO"];
